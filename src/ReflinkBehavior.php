66930dcf53a0a647d84f4c1589fb46292c42ffe7
Viacheslav Rudnev <slava.rudnev@gmail.com>
2014-12-02 17:50:16 +0200

    get & set

--- src/ReflinkBehavior.php
+++ src/ReflinkBehavior.php
@@ -13,11 +13,11 @@ class ReflinkBehavior extends \yii\base\Behavior
 
     public $refParamName = 'ref';
     public $refSessionName = 'referrerId';
-    
+
     public function events()
     {
         return [
-            \yii\web\Controller::EVENT_BEFORE_ACTION => 'getReferrerId',
+            \yii\web\Controller::EVENT_BEFORE_ACTION => 'setReferrerId',
         ];
     }
 
@@ -27,11 +27,18 @@ class ReflinkBehavior extends \yii\base\Behavior
      */
     public function getReferrerId()
     {
+        return \Yii::$app->session->get($this->refSessionName);
+    }
+
+    /**
+     * Get $refParamName from $_GET request and store to Session 
+     * @return string|null
+     */
+    public function setReferrerId()
+    {
         if (($referrerId = \Yii::$app->request->get($this->refParamName))) {
             \Yii::$app->session->set($this->refSessionName, $referrerId);
         }
-        
-        return \Yii::$app->session->get($this->refSessionName);
     }
 
 }
